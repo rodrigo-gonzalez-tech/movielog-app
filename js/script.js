64017a3de5a6fd5dd4bfa0b8357fd20652f7eb58
RodrigoG <rodrigo.gonzalez.tech@gmail.com>
2026-05-08 17:30:51 +0200

    Display popular tv shows

--- js/script.js
+++ js/script.js
@@ -36,10 +36,48 @@ async function displayPopularMovies() {
   });
 }
 
+// Display Popular TV Shows
+async function displayPopularShows() {
+  const { results } = await fetchAPIData("tv/popular");
+
+  results.forEach((show) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+    <a href="tv-details.html?id=${show.id}">
+            ${
+              show.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+                : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${show.name}</h5>
+            <p class="card-text">
+              <small class="text-muted">Air Date: ${show.first_air_date}</small>
+            </p>
+          </div>
+    `;
+    document.querySelector("#popular-shows").appendChild(div);
+  });
+}
+
+// Display Popular TV Shows
+
 // Fetch Data From TMDB API
 async function fetchAPIData(endpoint) {
   const API_KEY = "03ed3d1fc5098573034f4399c3d963eb";
   const API_URL = "https://api.themoviedb.org/3/";
+
+  showSpinner();
 
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`,
@@ -47,7 +85,18 @@ async function fetchAPIData(endpoint) {
 
   const data = await response.json();
 
+  hideSpinner();
+
   return data;
+}
+
+// Spinner
+function showSpinner() {
+  document.querySelector(".spinner").classList.add("show");
+}
+
+function hideSpinner() {
+  document.querySelector(".spinner").classList.remove("show");
 }
 
 // Highlight Active Page Link
@@ -68,14 +117,13 @@ function init() {
       console.log("Home");
       break;
     case "/movies.html":
-      console.log("Movies");
       displayPopularMovies();
       break;
     case "/movie-details.html":
       console.log("Movie Details");
       break;
     case "/shows.html":
-      console.log("TV Shows");
+      displayPopularShows();
       break;
     case "/tv-details.html":
       console.log("TV Details");
